3867cec92a0a11a6c3dd15743a51a41e5248e973
hoelsch <juergenhoelsch@gmail.com>
2023-06-14 22:25:28 +0200

    Simplify cryptos endpoint

--- cryptos/views.py
+++ cryptos/views.py
@@ -6,19 +6,20 @@ from .models import Crypto
 
 @require_http_methods(["GET"])
 def cryptos(request):
+    """
+    View function that returns a JSON response containing a list of supported cryptocurrencies.
+    Only authenticated users are allowed to access this view.
+
+    Returns:
+        JsonResponse: JSON response containing a list of supported cryptocurrencies.
+            The response structure is {"cryptos": [...]}.
+            Each cryptocurrency in the list is represented as a dictionary with keys "name",
+            "abbreviation", and "iconurl".
+    """
     if not request.user.is_authenticated:
         return JsonResponse({}, status=401)
 
-    queryset = Crypto.objects.all()
+    queryset = Crypto.objects.values("name", "abbreviation", "iconurl")
+    result = list(queryset)
 
-    cryptos = []
-    for crypto in queryset:
-        cryptos.append(
-            {
-                "name": crypto.name,
-                "abbreviation": crypto.abbreviation,
-                "iconurl": crypto.iconurl,
-            }
-        )
-
-    return JsonResponse({"cryptos": cryptos})
+    return JsonResponse({"cryptos": result})
