792628ca64bffe86edad5243b43c2a95df163365
hoelsch <juergenhoelsch@gmail.com>
2023-07-15 22:59:00 +0200

    Refactor login unit tests

--- auth/tests/test_login.py
+++ auth/tests/test_login.py
@@ -16,65 +16,55 @@ class LoginTestCase(TestCase):
         )
         self.user_id = user.id
 
-    def test_successful_login(self):
+    def _login(self, username, password):
         response = self.client.post(
             reverse("login"),
             data=json.dumps(
                 {
-                    "username": self.username,
-                    "password": self.password,
+                    "username": username,
+                    "password": password,
                 }
             ),
             content_type="application/json",
         )
+
+        return response
+
+    def test_successful_login(self):
+        response = self._login(self.username, self.password)
 
         self.assertEqual(response.status_code, 200)
         self.assertIn("sessionid", response.cookies)
         self.assertEqual(response["Content-Type"], "application/json")
 
         json_data = response.json()
+        expected_data = {"user_id": self.user_id}
 
-        self.assertDictContainsSubset({"user_id": self.user_id}, json_data)
+        self.assertDictContainsSubset(expected_data, json_data)
 
     def test_login_with_wrong_password(self):
-        response = self.client.post(
-            reverse("login"),
-            data=json.dumps(
-                {
-                    "username": self.username,
-                    "password": "wrong_password",
-                }
-            ),
-            content_type="application/json",
-        )
+        response = self._login(self.username, "wrong_password")
 
         self.assertEqual(response.status_code, 400)
         self.assertNotIn("sessionid", response.cookies)
         self.assertEqual(response["Content-Type"], "application/json")
 
         json_data = response.json()
+        expected_error = json_data["error"]
 
-        self.assertEqual(json_data["error"], "Invalid username or password")
+        self.assertEqual(expected_error, "Invalid username or password")
 
     def test_login_with_non_existing_user(self):
-        response = self.client.post(
-            reverse("login"),
-            data=json.dumps(
-                {
-                    "username": "non_existing_user",
-                    "password": self.password,
-                }
-            ),
-            content_type="application/json",
-        )
+        response = self._login("non_existing_user", self.password)
 
         self.assertEqual(response.status_code, 400)
         self.assertNotIn("sessionid", response.cookies)
         self.assertEqual(response["Content-Type"], "application/json")
 
         json_data = response.json()
+        expected_error = json_data["error"]
 
-        self.assertEqual(json_data["error"], "Invalid username or password")
+        self.assertEqual(expected_error, "Invalid username or password")
 
     def test_login_with_invalid_json_request_body(self):
         response = self.client.post(
